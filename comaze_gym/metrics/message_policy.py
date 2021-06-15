implementation of positive signalling metric. added mask-based computation of the pos. listening and ms-CIC metric. solved bug with computation of action's distribution when marginalising over messages...

comaze_gym/metrics/message_policy.py
@@ -3,9 +3,9 @@ from typing import List, Dict
 import torch
 import torch.nn as nn
 
-class ActionPolicy(nn.Module):
+class MessagePolicy(nn.Module):
     def __init__(self, model:nn.Module):
-        super(ActionPolicy, self).__init__()
+        super(MessagePolicy, self).__init__()
         self.model = model
 
     def parameters(self):
@@ -18,7 +18,7 @@ class ActionPolicy(nn.Module):
         raise NotImplementedError
 
     def clone(self, training=False):
-        return ActionPolicy(model=self.model.clone(training=training))
+        return MessagePolicy(model=self.model.clone(training=training))
 
     def forward(self, x:object):
         """
@@ -41,7 +41,7 @@ class ActionPolicy(nn.Module):
 
 from comaze_gym.utils import RuleBasedAgentWrapper
 
-class RuleBasedActionPolicy(ActionPolicy):
+class RuleBasedMessagePolicy(MessagePolicy):
     def __init__(
         self, 
         wrapped_rule_based_agent:RuleBasedAgentWrapper,
@@ -55,13 +55,13 @@ class RuleBasedActionPolicy(ActionPolicy):
             n= #messages * #actions.
             Else, n = # actions : directional actions.
         """
-        super(RuleBasedActionPolicy, self).__init__(
+        super(RuleBasedMessagePolicy, self).__init__(
             model=wrapped_rule_based_agent
         )
         self.combined_action_space = combined_action_space
     
     def clone(self, training=False):
-        return RuleBasedActionPolicy(
+        return RuleBasedMessagePolicy(
             wrapped_rule_based_agent=self.model.clone(training=training), 
             combined_action_space=self.combined_action_space
         )
@@ -90,14 +90,13 @@ class RuleBasedActionPolicy(ActionPolicy):
             -'infos': Dict containing the entry 'abstract_repr' that is
                 actually used by the :param model:RuleBasedAgentWrapper.
         
-        :return log_a:
-            torch.Tensor of logits over actions 
+        :return log_m:
+            torch.Tensor of logits over messages 
             (as a Discrete OpenAI's action space).
 
             Here, depending on :attr combined_action_space:,
-            we either marginalized over possible messages or not.
+            we either marginalized over possible actions or not.
         """
-
         actions_idx = self.model.take_action(**x)
         # batch_size x 1
 
@@ -105,21 +104,22 @@ class RuleBasedActionPolicy(ActionPolicy):
         self.action_space_dim = self.model.action_space_dim 
         
         # giving some entropy...
-        p_a = torch.ones((batch_size, self.action_space_dim)) #.to(actions_idx.device)
+        #p_m = torch.ones((batch_size, self.action_space_dim)) #.to(actions_idx.device)
+        p_m = torch.zeros((batch_size, self.action_space_dim)) #.to(actions_idx.device)
 
         for bidx in range(batch_size):
-            p_a[bidx, int(actions_idx[bidx])] = 2.0
+            p_m[bidx, int(actions_idx[bidx])] = 10.0
 
         if self.combined_action_space:
-            return p_a.log_softmax(dim=-1)
+            return p_m.log_softmax(dim=-1)
 
         # Otherwise, we sum over the messages dimension (excluding the NOOP action):
         self.vocab_size = (self.action_space_dim-1)//5
         # There are 5 possible directional actions:
-        p_a = p_a[...,:-1].reshape((batch_size, 5, self.vocab_size)).sum(dim=-1)
-        # batch_size x 5
+        p_m = p_m[...,:-1].reshape((batch_size, 5, self.vocab_size)).sum(dim=1)
+        # batch_size x vocab_size
 
-        return p_a.log_softmax(dim=1)
-
+        return p_m.log_softmax(dim=1)
+        
 
         

comaze_gym/metrics/multi_step_cic.py
@@ -84,6 +84,7 @@ class MultiStepCIC(object):
                 eff_mask = mask 
 
             for t in range(T):
+                m = eff_mask[actor_id][t] 
                 with torch.no_grad():
                     log_pt = action_policy(x[actor_id][t]).detach()
                     # batch_size x action_space_dim
@@ -98,11 +99,13 @@ class MultiStepCIC(object):
                 log_p_bar_t = self.action_policy_bar(xp[actor_id][t])
                 # batch_size x action_space_dim
 
-                nbr_samples += at.shape[0]
-                pred_at = log_p_bar_t.argmax(dim=-1)
-                accuracy += (pred_at==at).float().sum()
-
+                m = m.to(log_p_bar_t.device)
                 at = at.to(log_p_bar_t.device)
+                
+                nbr_samples += m*at.shape[0]
+                pred_at = log_p_bar_t.argmax(dim=-1)
+                accuracy += m*(pred_at==at).float().sum()
+
                 #L_ce_t = F.cross_entropy(
                 L_ce_t = F.nll_loss(
                     input=log_p_bar_t,
@@ -114,7 +117,7 @@ class MultiStepCIC(object):
                 #m = eff_mask[actor_id][t]
                 #L_ce[actor_id] += m*L_ce_t
                 if L_ce.device != L_ce_t.device:    L_ce = L_ce.to(L_ce_t.device)
-                L_ce += L_ce_t
+                L_ce += m*L_ce_t
                 # 1
 
             total_L_ce[actor_id] = L_ce
@@ -159,6 +162,7 @@ class MultiStepCIC(object):
 
         if biasing:
             action_policy = self.action_policy
+            self.action_policy.save_inner_state()
         else:
             action_policy = self.action_policy.clone()
             
@@ -173,21 +177,27 @@ class MultiStepCIC(object):
                 eff_mask = mask 
 
             for t in range(T):
+                m = eff_mask[actor_id][t] 
+                
                 pt = action_policy(x[actor_id][t]).exp()
                 # 1 x action_space_dim
                 p_bar_t = self.action_policy_bar(xp[actor_id][t]).exp().detach()
                 # 1 x action_space_dim
+                
+                m = m.to(p_bar_t.device)
+                pt = pt.to(p_bar_t.device)
                 L_pl_t = -(p_bar_t-pt).abs().sum()#.sum(dim=-1)
                 # 1
                 
                 #m = eff_mask[actor_id][t] 
                 #L_pl[actor_id] += m*L_pl_t
-                L_pl[actor_id] += L_pl_t
+                if L_pl.device != L_pl_t.device:    L_pl = L_pl.to(L_pl_t.device)
+                L_pl[actor_id] += m*L_pl_t
                 # batch_size
 
         if biasing:
             self.action_policy.reset(nbr_actors)
-        # TODO: reset the whole rnn states ...
+            self.action_policy.restore_inner_state()
 
         return L_pl
 
@@ -229,10 +239,14 @@ class MultiStepCIC(object):
                     eff_mask = mask 
 
                 for t in range(T):
+                    m = eff_mask[actor_id][t] 
+                    
                     log_pt = action_policy(x[actor_id][t])
                     # 1 x action_space_dim
                     log_p_bar_t = self.action_policy_bar(xp[actor_id][t])
                     # 1 x action_space_dim
+                    m = m.to(log_p_bar_t.device)
+                    log_pt = log_pt.to(log_p_bar_t.device)
                     ms_cic_t = F.kl_div(
                         input=log_pt,
                         target=log_p_bar_t.exp(),
@@ -243,7 +257,7 @@ class MultiStepCIC(object):
                     
                     #m = eff_mask[actor_id][t]
                     #ms_cic[actor_id] += m*ms_cic_t
-                    ms_cic[actor_id] += ms_cic_t
+                    ms_cic[actor_id] += m*ms_cic_t
                     # 1
 
         return ms_cic