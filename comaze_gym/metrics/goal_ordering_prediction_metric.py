OP debugging: DONE. + fixed issue with backpropagation through the agent's core of the auxiliary losses.

comaze_gym/metrics/goal_ordering_prediction_metric.py
@@ -19,7 +19,7 @@ from comaze_gym.metrics.hiddenstate_policy import HiddenStatePolicy
 
 
 class GoalOrderingPredictionMetric(object):
-    def __init__(self, hiddenstate_policy:HiddenStatePolicy, label_dim:int=5*4, data_save_path="./goal_ordering_module_save_path"):
+    def __init__(self, hiddenstate_policy:HiddenStatePolicy, label_dim:int=5*4, data_save_path="./goal_ordering_module_save_path", use_cuda=True):
         """
         
         :param hiddenstate_policy:
@@ -27,6 +27,8 @@ class GoalOrderingPredictionMetric(object):
             the inner hidden state representation of the agent.
 
         """
+        self.use_cuda = use_cuda 
+
         self.label_dim = label_dim+4*4
         self.hiddenstate_policy = hiddenstate_policy
         self.hidden_state_dim = self.hiddenstate_policy.get_hidden_state_dim()
@@ -37,6 +39,8 @@ class GoalOrderingPredictionMetric(object):
             nn.Linear(512,self.label_dim)
         ]
         self.prediction_net = nn.Sequential(*self.prediction_net)
+        
+        if self.use_cuda: self.prediction_net = self.prediction_net.cuda()
         print(self.prediction_net)
 
         self.iteration = 0 
@@ -233,6 +237,8 @@ class GoalOrderingPredictionMetric(object):
             """
             correct_pred_indices = torch.nonzero((per_actor_per_t_per_goal_acc[actor_id].sum(dim=-1)==4).float())
             # (min:0, max:timesteps x 1)
+            correct_pred_indices = correct_pred_indices.cpu()
+
             if correct_pred_indices.shape[0]>=1:
                 median_value = np.nanpercentile(
                     correct_pred_indices,
@@ -266,6 +272,8 @@ class GoalOrderingPredictionMetric(object):
 
             correct_pred_indices = torch.nonzero((per_actor_per_t_per_rule_acc[actor_id].sum(dim=-1)==4).float())
             # (min:0, max:timesteps x 1)
+            correct_pred_indices = correct_pred_indices.cpu()
+
             if correct_pred_indices.shape[0]>=1:
                 median_value = np.nanpercentile(
                     correct_pred_indices,

comaze_gym/metrics/message_trajectory_mutual_information_metric.py
@@ -43,7 +43,8 @@ class MessageTrajectoryMutualInformationMetric(object):
         batch_size = len(x)
         
         L_ps = torch.zeros(batch_size)
-        
+        ent_pi_m_x_it = []
+
         nbr_actors = self.message_policy.get_nbr_actor()
 
         if biasing:
@@ -76,11 +77,12 @@ class MessageTrajectoryMutualInformationMetric(object):
                 # 1 x message_space_dim
                 nbr_samples += pt.shape[0]
 
-                ent_pt = -sum([pt[...,i]*log_pt[...,i] for i in range(pt.shape[-1])])
+                ent_pt = -torch.sum(pt*log_pt, dim=-1, keepdim=True)
                 # 1 x 1
-                
+                ent_pi_m_x_it.append(ent_pt)
+
                 if self.target_ent_pt is None:
-                    self.target_ent_pt = 0.5*torch.log(pt.shape[-1]*torch.ones(1)).to(pt.device)
+                    self.target_ent_pt = 0.5*torch.log(pt.shape[-1]*torch.ones(1)).to(pt.device).detach()
 
                 L_ps_t = self.target_loss_lambda*torch.pow(ent_pt-self.target_ent_pt, 2.0)
                 # 1
@@ -88,6 +90,9 @@ class MessageTrajectoryMutualInformationMetric(object):
                 if L_ps.device != L_ps_t.device:    L_ps = L_ps.to(L_ps_t.device)
                 L_ps[actor_id:actor_id+1] += m*L_ps_t.reshape(-1)
                 # batch_size
+            
+            # normalising:
+            L_ps[actor_id:actor_id+1] /= T 
 
         # normalization:
         pi_bar_m = pi_bar_m/nbr_samples
@@ -95,15 +100,22 @@ class MessageTrajectoryMutualInformationMetric(object):
         log_pi_bar_m = pi_bar_m.log()
         # 1 x message_space_dim
         
-        ent_pi_bar_m = -sum([pi_bar_m[...,i]*log_pi_bar_m[...,i] for i in range(pi_bar_m.shape[-1])])
+        ent_pi_bar_m = -torch.sum(pi_bar_m*log_pi_bar_m, dim=-1, keepdim=True)
         # 1 x 1
         
-        # Loss is minimized when averaged entropy is maximized:
-        L_ps = L_ps-nbr_samples*ent_pi_bar_m.reshape(-1)
+        # Mutual information between agent's messages and trajectories:
+        ent_pi_m_x_it = torch.cat(ent_pi_m_x_it, dim=-1)
+        # (1 x nbr_samples)
+        exp_ent_pi_m_x_it_over_x_it = ent_pi_m_x_it.mean(dim=-1, keepdim=True)
+        # (1 x 1 )
+
+        # Loss is minimized when averaged policy's entropy is maximized:
+        #L_ps = L_ps-nbr_samples*ent_pi_bar_m.reshape(-1)
+        L_ps = L_ps-ent_pi_bar_m.reshape(-1)
         # batch_size 
 
         if biasing:
             self.message_policy.reset(nbr_actors, training=True)
             self.message_policy.restore_inner_state()
 
-        return L_ps, ent_pi_bar_m
+        return L_ps, ent_pi_bar_m, exp_ent_pi_m_x_it_over_x_it

comaze_gym/utils/wrappers.py
@@ -1,3 +1,5 @@
+from typing import Dict, List 
+
 import copy
 import numpy as np
 import gym
@@ -368,9 +370,10 @@ class DiscreteCombinedActionWrapper(gym.Wrapper):
 
         return observations, copy.deepcopy(self.infos) 
 
-    def _decode_action(self, action):
-        player_on_turn = self.infos[0]["current_player"].item()
-        action = action[player_on_turn].item()
+    def _decode_action(self, action:np.ndarray)->Dict[str,object]:
+        #player_on_turn = self.infos[0]["current_player"].item()
+        #action = action[player_on_turn].item()
+        action = action.item()
 
         if not self.action_space.contains(action):
             raise ValueError('action {} is invalid for {}'.format(action, self.action_space))
@@ -394,10 +397,9 @@ class DiscreteCombinedActionWrapper(gym.Wrapper):
         
         return ad 
 
-    def _encode_action(self, action_dict):
+    def _encode_action(self, action_dict:Dict[str,object])->int:
         original_action_direction_id = action_dict['directional_action']
         original_action_sentence = action_dict['communication_channel']
-        import ipdb; ipdb.set_trace()
         original_action_sentence_id = self.sentence2sentenceId[ original_action_sentence.tostring() ]
 
         if original_action_sentence==0 and original_action_direction_id==4:
@@ -407,7 +409,10 @@ class DiscreteCombinedActionWrapper(gym.Wrapper):
 
         return encoded_action 
     
-    def step(self, action):
+    def step(self, action:List[np.ndarray]):
+        player_on_turn = self.infos[0]["current_player"].item()
+        action = action[player_on_turn]
+
         original_action = self._decode_action(action)
 
         next_observations, reward, done, next_infos = self.env.step(original_action)